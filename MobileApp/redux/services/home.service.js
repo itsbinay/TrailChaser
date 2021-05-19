205361e4feb39ceb9fdc6e86364f46b0b0a9843d
Kashish Jagtiani <kashish_jagtiani@hotmail.com>
2021-05-19 15:09:56 +0800

    commit

--- MobileApp/redux/services/home.service.js
+++ MobileApp/redux/services/home.service.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-const API_URL = "https://10.0.2.2:5000"
+const API_URL = "http://10.0.2.2:5000"
 
 export const homeService = {
     fetchAllTrails
@@ -8,16 +8,31 @@ export const homeService = {
 async function fetchAllTrails(){
     
     try{
+        // console.log("enter:")
+        // const returned_promise = Axios.get(API_URL + '/getTrails')
+        
+        // const response = await returned_promise
+        // console.log("here", response)
+        // const result = response.data.result
+        // return result
+
         console.log("enter")
-        const returned_promise = Axios.get(
-            API_URL + '/getTrails'
-        )
-        console.log("ERROR")
-        const response = await returned_promise
-        const result = response.data.result
-        return result
+        fetch('http://10.0.2.2:5000/getTrails',{
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+        })
+        .catch(err => console.error("err1", err))
+        .then(response => response.json())
+        .catch(err => console.error("err2", err))
+        .then(data => {
+            console.log("here: ", data);
+        })
+        .catch(err => console.error("err3", err));
 
     }catch (err) {
         console.log("err", err)
-      }
+    }
 }
